1912b05dfa17cff7db6b069c464eb6f1397ab082
niti34ds <nitinsdl501@gmail.com>
2024-04-20 14:53:07 +0530

    first

---
+++ README.md
@@ -0,0 +1,48 @@
+# Job-Portal
+
+The online job portal application allows job seekers and recruiters to connect.The application provides the ability for job seekers to create their accounts, upload their profile and resume, search for jobs, apply for jobs, view different job openings. The application provides the ability for companies to create their accounts, search candidates, create job postings, and view candidates applications.
+
+# View Tutorials
+
+Job Portal (PHP) - Part 1 - Introduction & User Login/Registration - https://youtu.be/3VGxEEpWEaw
+
+Job Portal (PHP) - Part 2 - User Dashboard & Profile Updating - https://youtu.be/VkKXaqFafRA
+
+Job Portal (PHP) - Part-3 - Company Login/Registration & Creating Job Posts - https://youtu.be/OiapxMhgH_I
+
+Job Portal (PHP) - Part 4 - View/Edit/Delete Job Posts - https://youtu.be/E1F-snEgpPs
+
+Job Portal (PHP) - Part 5 - Showing Random Job Post On Homepage & User Applying To Job - https://youtu.be/3qmjqLx3o6o
+
+Job Portal (PHP) - Part 6 - Code Refactor & Bug Fix - https://youtu.be/YVv_JCcVb7s
+
+Job Portal (PHP) - Part 7 - Job Search Page With Advanced Searching Options  - https://youtu.be/wgNd-HgxqJg
+
+Job Portal (PHP) - Part 8 - Generating Resume By Filling Form Using Custom Resume Templates - https://youtu.be/Wndjgmk1SyM
+
+Job Portal (PHP) - Part 9 - Uploading Resume & Company View/Reject User Application - https://youtu.be/SMjiO0UwAoA
+
+Job Portal (PHP) - Part 10 - Admin Panel - https://youtu.be/UdLpBUR2lCs
+
+Job Portal (PHP) - Part 11 - Admin Verifying Company & User Email Verification - https://youtu.be/RwybCIHj0JE
+
+Job Portal (PHP) - Part 12 - Validations & Code Improvements - https://youtu.be/mpO1j16udzM
+
+Job Portal (PHP) - Part 13 - Email Sending Bug Fix - https://youtu.be/5ONR88bt0bY
+
+Job Portal (PHP) - Part 14 - Search Highlighting & Forgot Password - https://youtu.be/HR7tvK5oO1g
+
+Job Portal (PHP) - Part 15 - Select Company Location & Fixed Experience Filter - https://youtu.be/0mDXlpOulWc
+
+Job Portal (PHP) - Part 16 - Theme Update Introduction - https://youtu.be/Lm5I6SSgJAw
+
+Job Portal (PHP) - Part 17 - Homepage, Login & Registration Updated To New Theme - https://youtu.be/6neTJ6QRH6U
+
+Job Portal (PHP) - Part 18 - Company Registration & Login Updated To New Theme - https://youtu.be/WJrQTybFQxo
+
+Job Portal (PHP) - Part 19 - Updated Create Job Post & View Company Job Post - https://youtu.be/PK3Q9aAba-0
+
+
+# Contact
+Feel free to contact me through my website http://learningfromscratch.online/ 
+>If you are following this tutorial series I would appritiate if you can subscribe and like my videos as it will keep me motivated to share my knowledge with you all!
---
+++ login.php
@@ -0,0 +1,152 @@
+<?php
+  //To Handle Session Variables on This Page
+  session_start(); 
+
+  //If user is already logged in then redirect them back to dashboard. 
+  //This is required if user tries to manually enter login.php in URL.
+  if(isset($_SESSION['id_user'])) {
+    header("Location: user/dashboard.php");
+    exit();
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Job Portal</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <!-- NAVIGATION BAR -->
+    <header>
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Job Portal</a>
+          </div>
+
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">     
+            <ul class="nav navbar-nav navbar-right">
+            <?php
+            //Show user dashboard link once logged in.
+            //Todo: Check if Company logged in then show company dashboard? 
+            if(isset($_SESSION['id_user'])) { 
+              ?>
+              <li><a href="user/dashboard.php">Dashboard</a></li>
+              <li><a href="logout.php">Logout</a></li>
+            <?php
+            } else { 
+              //Show Login Links if no one is logged in.
+            ?>
+              <li><a href="company.php">Company</a></li>
+              <li><a href="register.php">Register</a></li>
+              <li><a href="login.php">Login</a></li>
+            <?php } ?>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
+    </header>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-md-offset-4 well">
+          <h2 class="text-center">Login</h2>
+            <form method="post" action="checklogin.php">
+              <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email" required="">
+              </div>
+              <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required="">
+              </div>
+              <div class="form-group">
+                <a href="forgot-password.php">Forgot Password</a>
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn btn-success">Submit</button>
+              </div>
+              <?php 
+              //If User have successfully registered then show them this success message
+              //Todo: Remove Success Message without reload?
+              if(isset($_SESSION['registerCompleted'])) {
+                ?>
+                <div>
+                  <p id="successMessage" class="text-center">Check your email!</p>
+                </div>
+              <?php
+               unset($_SESSION['registerCompleted']); }
+              ?>   
+              <?php 
+              //If User Failed To log in then show error message.
+              if(isset($_SESSION['loginError'])) {
+                ?>
+                <div>
+                  <p class="text-center">Invalid Email/Password! Try Again!</p>
+                </div>
+              <?php
+               unset($_SESSION['loginError']); }
+              ?>      
+
+              <?php 
+              //If User Failed To log in then show error message.
+              if(isset($_SESSION['userActivated'])) {
+                ?>
+                <div>
+                  <p class="text-center">Your Account Is Active. You Can Login</p>
+                </div>
+              <?php
+               unset($_SESSION['userActivated']); }
+              ?>    
+
+               <?php 
+              //If User Failed To log in then show error message.
+              if(isset($_SESSION['loginActiveError'])) {
+                ?>
+                <div>
+                  <p class="text-center">Your Account Is Not Active. Check Your Email.</p>
+                </div>
+              <?php
+               unset($_SESSION['loginActiveError']); }
+              ?>        
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+      $(function() {
+        $("#successMessage:visible").fadeOut(2000);
+      });
+    </script>
+  </body>
+</html>
